9e294f5946afe5e16f17c618918446d5db41db1b
DHQ <56886743+dhq-github2019@users.noreply.github.com>
2024-10-10 22:57:40 +0700

    Update resources.py

--- starter/backend/movies/resources.py
+++ starter/backend/movies/resources.py
@@ -3,9 +3,9 @@ from flask.views import MethodView
 
 # Dummy database to hold movie examples
 movies = {
-    "123":{"title":"Top Gun: Maverick", "description":"Fighter planes"},
-    "456":{"title":"Sonic the Hedgehog", "description":"Blue Sega character"},
-    "789":{"title":"A Quiet Place", "description":"Scary monsters"},
+    "123": {"title":"Top Gun: Maverick", "description":"Fighter planes"},
+    "456": {"title":"Sonic the Hedgehog", "description":"Blue Sega character"},
+    "789": {"title":"A Quiet Place", "description":"Scary monsters"},
 }
 
 
